ad8b020911eccd3c6b32c854eeb4af26cb7f550f
aln-liang <alan.liangw@gmail.com>
2023-01-22 19:51:41 -0500

    Changed HTML attribute attribute on website has changed added print statement to attrieve image link

--- web_scrape.py
+++ web_scrape.py
@@ -5,7 +5,8 @@ github_user = input('Input Github User: ')
 url = f'https://github.com/{github_user}'
 r = requests.get(url)
 soup = bs(r.content, 'html.parser')
-profile_image = soup.find('img', {'alt': 'Avatar'})
+profile_image = soup.find('img', {'width' : '260'})['src']
+print(profile_image)
 
 
 
